4a9fdac13d7358b31427fe9ab51162b350433d88
devKiratu <dev.kiratu@gmail.com>
2023-06-05 14:17:07 +0300

    configure url mapping

--- index.php
+++ index.php
@@ -3,6 +3,13 @@ require_once('utils.php');
 spl_autoload_register('load_class');
 header("Content-type: application/json; charset=UTF-8");
 
+$url_parts = explode("/", $_SERVER['REQUEST_URI']);
+
+if ($url_parts[1] != "products") {
+  http_response_code(404);
+  exit;
+}
+
 $database = new \Models\Database("localhost", "demo-app", "root");
 $pdo = $database->getConnection();
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
