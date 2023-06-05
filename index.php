44d5dca58d0ece47a242ed03a45fc9db21615507
devKiratu <dev.kiratu@gmail.com>
2023-06-05 13:07:03 +0300

    add Database class

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php
-echo "Hello, world!";
+require_once('utils.php');
+spl_autoload_register('load_class');
+
+$database = new \Models\Database("localhost", "demo-app", "root");
+$pdo = $database->getConnection();
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
