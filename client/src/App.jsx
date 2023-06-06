c85cf69bf53fe29d8c385cb5ea3b7311f14002bf
devKiratu <dev.kiratu@gmail.com>
2023-06-06 08:33:17 +0300

    bootstrap react app

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>General Store</h1>
+      <div className="card">
+        <p>Welcome to our product shop.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
